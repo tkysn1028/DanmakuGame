8b698dea3d7080c17960e0305a0cf1d740191f72
tkysn1028 <tkysn1028@gmail.com>
2026-08-23 16:36:59 +0900

    HPが０になったらボスがステージから消えるように修正

--- game/entity/Boss.java
+++ game/entity/Boss.java
@@ -3,12 +3,16 @@ package game.entity;
 import game.util.ConfigConst;
 
 public class Boss extends Entity {
-    private int hitPoints = 50;
+    private int hitPoints = 2;
     public int hitPoints() { return hitPoints; }
     public void hit() {
-        if(hitPoints > 0) hitPoints--;
+        if (hitPoints > 0) hitPoints--;
     }
+    private double retreatSpeed = 0.0;
+    private static final double retreatAccelSpeed = 0.02;
 
+    public boolean isGone() { return y < -radius * 2; }
+    
     public Boss() {
         x = ConfigConst.WIDTH / 2.0;
         y = ConfigConst.HEIGHT / 5.0;
@@ -16,6 +20,11 @@ public class Boss extends Entity {
     }
 
     public void update(int frame) {
-        x = ConfigConst.WIDTH / 2.0 + Math.sin(frame / 50.0) * ConfigConst.WIDTH / 4.0;
+        x = ConfigConst.WIDTH / 2.0 + Math.sin(frame / 50.0) * ConfigConst.WIDTH / 4.0;;
+
+        if (hitPoints <= 0) {
+            retreatSpeed += retreatAccelSpeed;
+            y -= retreatSpeed;
+        }
     }
 }
