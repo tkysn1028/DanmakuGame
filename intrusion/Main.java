01e93b8e01a8d00dabd53ed5a69fbdadabef8a81
tkysn1028 <tkysn1028@gmail.com>
2026-08-30 23:28:58 +0900

    123

---
+++ intrusion/Main.java
@@ -0,0 +1,20 @@
+package intrusion;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            var frame = new JFrame();
+            // var panel = new GamePanel();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            // frame.add(panel);
+            frame.pack();
+            frame.setResizable(false);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+            // panel.requestFocusInWindow();
+        });
+    }
+}
